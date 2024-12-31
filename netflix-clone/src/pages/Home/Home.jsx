60032eee96eb1811e991e5c8551765200edb49a7
ALAZAR BIRHANE <you@example.com>
2024-12-31 20:16:17 +0300

    Row

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../../component/Header/Header'
 import Footer from '../../component/Footer/Footer'
 import Banner from '../../component/Banner/Banner'
+import Rowlist from '../../component/Rows/RowList/Rowlist'
 
 const Home = () => {
   return (
     <>
-      <Header/>
-      <Banner/>
-      <Footer/>
+      <Header />
+      <Banner />
+      <Rowlist />
+      <Footer />
     </>
   )
 }
